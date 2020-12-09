b9ce90a31232a706e0a2d7b350dea75c2654a47e
eniyavan <71455480+eniyavanweb@users.noreply.github.com>
2020-12-09 08:07:45 -0800

    spicyo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path:'home',component:HomeComponent}
+
+]; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, MatToolbarModule, MatIconModule,MatButtonModule,RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
