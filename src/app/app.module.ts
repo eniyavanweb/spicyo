34d718b6b247462367caa740534e1fc2e372b5ea
eniyavan <71455480+eniyavanweb@users.noreply.github.com>
2020-12-12 01:31:30 -0800

    comment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { OurclientComponent } from './ourclient/ourclient.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
+  {path:'',component:HomeComponent},
   {path:'home',component:HomeComponent},
   {path:'about',component:AboutComponent},
   {path:'ourclient',component:OurclientComponent},
